07ec5d38edb8e6cb920bdf24588717c4d6238f43
maryamissam123 <maryamissam123@gmail.com>
2026-05-21 18:02:22 +0200

    build: test E2E test and add to cypress

---
+++ cypress/e2e/user-flows.cy.js
@@ -0,0 +1,33 @@
+describe('User Flows', () => {
+  // Test 1: Login flow
+  it('1. Bruger skal kunne logge ind', () => {
+    cy.visit('/login');
+    cy.get('input[type="email"]').type('test@milton.dk');
+    cy.get('input[type="password"]').type('123456');
+    cy.get('button#login').click();
+    cy.url().should('include', '/dashboard');
+  });
+
+  // Test 2: Beskyttelse af ruter
+  it('2. Bruger skal sendes til login hvis ikke logget ind', () => {
+    cy.visit('/manager/projects');
+    cy.url().should('include', '/login');
+  });
+
+  // Test 3: Navigation i projekter
+  it('3. Bruger skal kunne klikke på et projekt og se detaljer', () => {
+    cy.visit('/dashboard');
+    cy.get('.project-card').first().click();
+    cy.url().should('contain', '/project/');
+  });
+
+  // Test 4: Logout
+  it('4. Bruger skal kunne logge ud', () => {
+    // Først log ind
+    cy.visit('/login');
+    cy.get('button#login').click();
+    // Så log ud
+    cy.get('#logout-btn').click();
+    cy.url().should('include', '/login');
+  });
+});
